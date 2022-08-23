1d8589e53e3514131448f994f0c8c2895a584c76
mark1879 <mark1879@163.com>
2022-08-24 07:15:56 +0800

    增加“跳跃游戏2”

--- run.sh
+++ run.sh
@@ -17,3 +17,4 @@ ${build_dir_path}/bin/test_add_two_numbers
 ${build_dir_path}/bin/test_length_of_longest_sub_string
 ${build_dir_path}/bin/test_max_area
 ${build_dir_path}/bin/test_can_jump
+${build_dir_path}/bin/test_jump
